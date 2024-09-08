e4d532e125e9a0e13f884fda85e12b011673df19
learnRight <connect2gracealone@gmail.com>
2024-09-08 17:51:31 +0100

    Added person class with introduce method

---
+++ person.py
@@ -0,0 +1,17 @@
+# Define the Person class
+class Person:
+    # Constructor to initialize attributes
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    # Method to introduce the person
+    def introduce(self):
+        print(f"Hello, my name is {self.name}. I am {self.age} years old and I identify as {self.gender}.")
+
+# Create an instance of the Person class
+person1 = Person("Ezekiel Samuel", 25, "Male")
+
+# Call the introduce method
+person1.introduce()
